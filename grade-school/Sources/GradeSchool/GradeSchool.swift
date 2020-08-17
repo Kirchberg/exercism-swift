f19b8e5670b93d24d8c2f81840d8a4a2738fcd89
Kirchberg <kirill.kostarev.2017@gmail.com>
2020-08-17 12:26:31 +0300

    Lazy stored property inappropriate in this exercise

--- grade-school/Sources/GradeSchool/GradeSchool.swift
+++ grade-school/Sources/GradeSchool/GradeSchool.swift
@@ -1,20 +1,20 @@
 class GradeSchool {
     
-    var rosterOfStudents: [Int: [String]] = [:]
+    private var rosterOfStudents: [Int: [String]] = [:]
     
-    lazy var roster: [Int: [String]] = {
-        return rosterOfStudents
-    }()
+    var roster: [Int: [String]] {
+        rosterOfStudents
+    }
     
-    lazy var sortedRoster: [Int: [String]] = {
-        return rosterOfStudents.mapValues {$0.sorted(by: <)}
-    }()
+    var sortedRoster: [Int: [String]] {
+        rosterOfStudents.mapValues {$0.sorted(by: <)}
+    }
     
     func addStudent(_ nameOfStudent: String, grade number: Int) {
         rosterOfStudents[number, default: []].append(nameOfStudent)
     }
     
     func studentsInGrade(_ grade: Int) -> [String] {
-        return rosterOfStudents[grade, default: []]
+        rosterOfStudents[grade, default: []]
     }
 }
