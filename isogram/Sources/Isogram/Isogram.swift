6dd977d49ab87f94b119969cc55bc06d92edafe4
Kirchberg <kirill.kostarev.2017@gmail.com>
2020-08-18 12:53:35 +0300

    Initial commit

--- isogram/Sources/Isogram/Isogram.swift
+++ isogram/Sources/Isogram/Isogram.swift
@@ -1,5 +1,18 @@
 struct Isogram {
+    private static let punctuation: Set<Character> = [" ", "-"]
     static func isIsogram(_ word: String) -> Bool {
-        return true
+        var isIsogram: Bool = true
+        var setOfUniqueCharacters: Set<Character> = []
+        var wordWithoutPunctuation = word.lowercased()
+        wordWithoutPunctuation.removeAll(where: { punctuation.contains($0) })
+        for char in wordWithoutPunctuation {
+            if !setOfUniqueCharacters.contains(char) {
+                setOfUniqueCharacters.insert(char)
+            } else {
+                isIsogram = false
+                break
+            }
+        }
+        return isIsogram
     }
 }
