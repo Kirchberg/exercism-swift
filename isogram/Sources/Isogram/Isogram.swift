5599c72bfbe16714a71aa8ef973ed60f21eadcff
Kirchberg <kirill.kostarev.2017@gmail.com>
2020-08-28 20:48:41 +0300

    Less operations, more speed

--- isogram/Sources/Isogram/Isogram.swift
+++ isogram/Sources/Isogram/Isogram.swift
@@ -1,16 +1,8 @@
 struct Isogram {
     static func isIsogram(_ word: String) -> Bool {
-        var isIsogram: Bool = true
         var setOfUniqueCharacters: Set<Character> = []
         var wordWithoutPunctuation = word.lowercased()
         wordWithoutPunctuation.removeAll(where: { [" ", "-"].contains($0) })
-        for char in wordWithoutPunctuation {
-            if !setOfUniqueCharacters.allSatisfy({$0 != char}) {
-                isIsogram = false
-                break
-            }
-            setOfUniqueCharacters.insert(char)
-        }
-        return isIsogram
+        return wordWithoutPunctuation.allSatisfy({setOfUniqueCharacters.insert($0).inserted})
     }
 }
