94cbdf0896ba9da1dbff2a7a6d67d64f54e046da
Kirchberg <kirill.kostarev.2017@gmail.com>
2020-09-12 21:15:10 +0300

    Less code, less complexity, more efficiency

--- phone-number/Sources/PhoneNumber/PhoneNumber.swift
+++ phone-number/Sources/PhoneNumber/PhoneNumber.swift
@@ -6,22 +6,28 @@ extension String {
     }
 }
 
+extension Substring {
+    func toString() -> String {
+        String(self)
+    }
+}
+
 extension PhoneNumber: CustomStringConvertible{
     var description: String {
         return "(\(number.prefix(3))) \(number.dropLast(4).suffix(3))-\(number.suffix(4))"
     }
 }
 
-class PhoneNumber{
-    var number: String
+struct PhoneNumber{
+    let number: String
     let areaCode: String
     init(_ startingNumber: String) {
         var sourceNumber = startingNumber
         
         sourceNumber.removeCountryCode()
-        var telephoneNumber = Array(sourceNumber)
-        areaCode = String(sourceNumber.prefix(3))
-        telephoneNumber.removeAll(where: {["-", " ", "(", ")", "."].contains($0)})
-        number = (telephoneNumber.count <= 9 || telephoneNumber.count >= 11) ? String(repeating: "0", count: 10) : String(telephoneNumber)
+        var telephoneNumber = sourceNumber
+        areaCode = sourceNumber.prefix(3).toString()
+        telephoneNumber.removeAll(where: {("- ().").contains($0)})
+        number = (telephoneNumber.count != 10) ? String(repeating: "0", count: 10) : telephoneNumber
     }
 }
