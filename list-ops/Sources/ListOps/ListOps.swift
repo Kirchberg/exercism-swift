529f8a6de7e0632a382f2e9a8b83524e3bab4864
Kirill Kostarev <kirill.kostarev.2017@gmail.com>
2020-12-17 16:39:20 +0300

    Almost done

--- list-ops/Sources/ListOps/ListOps.swift
+++ list-ops/Sources/ListOps/ListOps.swift
@@ -1,4 +1,5 @@
 struct ListOps {
+    
     static func append<T>(_ firstArray: [T], _ secondArray: [T]) -> [T] {
         return firstArray + secondArray
     }
@@ -37,7 +38,29 @@ struct ListOps {
         return newArray
     }
     
-    static func foldLeft(_ sourceArray: [Int], accumulated: Int, combine:  ) -> Int {
-        return 0 
+    static func foldLeft(_ sourceArray: [Int], accumulated: Int, combine:  (Int, Int) -> Int) -> Int {
+        var sum: Int = 0
+        for elem in sourceArray {
+            sum += elem
+        }
+        return combine(accumulated, sum)
+    }
+
+    static func foldRight<T>(_ sourceArray: [T], accumulated: T, combine: (T,T) -> T) -> T {
+        var sum: T
+        for elem in sourceArray where T.self == Int.self {
+            sum += elem
+        }
+        return combine(sum, accumulated)
+    }
+    
+    static func reverse<T>(_ sourceArray: [T]) -> [T] {
+        var newArray = [T]()
+        let count = ListOps.length(sourceArray) - 1
+        guard count > 0 else { return [] }
+        for index in stride(from: count, through: 0, by: -1) {
+            newArray += [sourceArray[index]]
+        }
+        return newArray
     }
 }
